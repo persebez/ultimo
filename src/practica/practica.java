a89b38001821aa7e1941261a4e63eb60f14467b6
Persebez2 <persebez2@hotmail.com>
2016-06-18 00:39:39 -0500

    Tercer commit

--- src/practica/practica.java
+++ src/practica/practica.java
@@ -7,6 +7,8 @@ public class practica {
     public static void main(String[] args) {
         
         System.out.println("Hola como estas");
+        
+        System.out.println("Segunda linea");
     }
 
 
